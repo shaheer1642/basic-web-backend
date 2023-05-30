960c4e120ca602e632bfdbb9ea8c57b76bf36502
MrSofty123 <shaheer1642@gmail.com>
2023-05-30 23:30:25 +0500

    test

---
+++ db_module.js
@@ -0,0 +1,22 @@
+
+function db_users() {
+    const users = [{
+      name: 'mody',
+      age: '20'
+    }, {
+      name: 'softy',
+      age: '22'
+    }, {
+        name: 'softy1',
+        age: '22'
+    }, {
+        name: 'softy2',
+        age: '22'
+    }]
+  
+    return users;
+}
+
+module.exports = {
+    db_users
+}
--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3001
 const path = require('path');
+const { db_users } = require('./db_module');
+const cors = require('cors')
+const bodyParser = require('body-parser');
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/index.html'));
+app.use(cors())
+app.use(bodyParser.json());
+
+app.get('/api/getdata/:id', (req, res) => {
+  console.log('id api called')
+  console.log(req.params)
 })
+app.get('/api/getdata/:id/:username', (req, res) => {
+  console.log('id, username api called')
+  console.log(req.params)
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
